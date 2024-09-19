2a86f4dff14c38128953f525916ac5dbe18ce803
Nischal Shrestha <nischalshrestha324@gmail.com>
2024-09-19 15:11:57 +0545

    drag and drop added

---
+++ js/app.js
@@ -0,0 +1,95 @@
+const taskLists = document.querySelectorAll('.task-list');
+const backlogTasks = document.querySelector('#backlog .task-list');
+
+let tasks = [
+    {
+        id: 0,
+        title: "Fix submit button",
+        description: "The submit button has stoped working since the last release."
+    },
+    {
+        id: 1,
+        title: "Change text on T and C's",
+        description: "The terms and conditions need updating as per the business meeting."
+    },
+    {
+        id: 2,
+        title: "Change banner picture",
+        description: "Marketing has requested a new banner to be added to the website."
+    }
+];
+
+taskLists.forEach(taskList => {
+    taskList.addEventListener('dragover', dragOver)
+    taskList.addEventListener('drop', dragDrop)
+})
+
+function createTask(taskId, title, description) {
+    const taskCard = document.createElement('div');
+    const taskHeader = document.createElement("div");
+    const taskTitle = document.createElement('p');
+    const taskDescriptionContainer = document.createElement('div');
+    const taskDescription = document.createElement('p');
+    const deleteIcon = document.createElement('p');
+
+    taskCard.classList.add('task-container')
+    taskHeader.classList.add('task-header');
+    taskDescriptionContainer.classList.add('task-description-container');
+
+    taskTitle.textContent = title;
+    taskDescription.textContent = description;
+    deleteIcon.textContent = "☒"
+
+    taskCard.setAttribute('draggable', true);
+    taskCard.setAttribute('task-id', taskId)
+
+    taskCard.addEventListener('dragstart', dragStart);
+
+    taskDescriptionContainer.append(taskDescription);
+    taskHeader.append(taskTitle, deleteIcon)
+    taskCard.append(taskHeader, taskDescriptionContainer)
+    backlogTasks.append(taskCard);
+}
+
+function addColor(column) {
+    let color;
+    switch(column) {
+        case 'backlog':
+            color = 'rgb(96, 96, 192)'
+            break
+        case 'doing':
+            color = 'rgb(83, 156, 174)'
+            break
+        case 'done':
+            color = 'rgb(224, 165, 116)'
+            break
+        case 'discard':
+            color = 'rgb(222, 208, 130)'
+            break
+        default:
+            color = 'rgb(232, 232, 232)'
+    }
+    return color;
+}
+
+function addTasks() {
+    tasks.forEach(task => createTask(task.id, task.title, task.description))
+}
+
+addTasks();
+
+let elementBeingDragged;
+
+function dragStart() {
+    elementBeingDragged = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragDrop() {
+    let columnId = this.parentNode.id;
+    elementBeingDragged.firstChild.style.backgroundColor = addColor(columnId)
+    this.append(elementBeingDragged);
+}
